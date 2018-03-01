c9b9189bc835ccb862927042204640560ffa0786
Anna Sesselmann <anna.sesselmann@tum.de>
2018-03-01 19:16:02 +0100

    reading file works

--- hashcode.py
+++ hashcode.py
@@ -1,4 +1,14 @@
 # read input file
+file = open('./samples/a_example.in', 'r')
+line =  file.readline()
+R,C,F,N,B,T = line.split()
+
+rides = []
+for line in file:
+	ride = [int(r) for r in line.split()]
+	rides.append(ride)
+
+print rides
 
 # calculate solution
 
