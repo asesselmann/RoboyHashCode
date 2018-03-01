9f7a698f6df4c06a02d11119b7e1c74a0b267481
Anna Sesselmann <anna.sesselmann@tum.de>
2018-03-01 21:38:31 +0100

    x

--- vehicle.py
+++ vehicle.py
@@ -7,10 +7,11 @@ class Vehicle:
 
     def end_time(self):
         distance = 0
-        distance += ride.l1_norm(0, 0, self.rides[0].start_row, self.rides[0].start_col)
+        to_next = ride.l1_norm(0, 0, self.rides[0].start_row, self.rides[0].start_col)
+        distance += to_next if to_next >= self.rides[0].start else self.rides[0].start
         distance += self.rides[0].distance
         for i in range(1, len(self.rides)):
-            distance += ride.l1_norm(self.rides[i-1].end_row, self.rides[i-1].end_col, self.rides[i].start_row, self.rides[i].start_col)
+        	to_next = ride.l1_norm(self.rides[i-1].end_row, self.rides[i-1].end_col, self.rides[i].start_row, self.rides[i].start_col)
+            distance += to_next if to_next >= self.rides[i].start else self.rides[i].start
             distance += self.rides[i].distance
-
         return distance
